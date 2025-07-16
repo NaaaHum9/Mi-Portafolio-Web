dfc13134628a45d7c56950b27eaea2eaf7a2d645
NaaaHum9 <vgjn98x@gmail.com>
2025-07-16 12:41:45 -0600

    Hola mundo

--- backend/app.py
+++ backend/app.py
@@ -10,6 +10,10 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+@app.route('/')
+def hello():
+    return "Hola mundo"
+
 @app.route('/projects')
 def get_projects():
     conn = get_db_connection()
